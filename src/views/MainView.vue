b68b8c4af095696c15428c1e68b90f94e17ce5f3
ratih shinta <rstihshintaaa6@gmail.com>
2024-01-16 15:48:48 +0700

    fix [RS] : fixing conflict

--- src/views/MainView.vue
+++ src/views/MainView.vue
@@ -7,7 +7,7 @@
     <location-comp/>
     <kerajinan-comp/>
     <quote-comp/>
-    <f-a-q-comp/>
+    <!-- <f-a-q-comp/> -->
     <footer-comp/>
   </div>
 </template>
@@ -20,15 +20,15 @@ import ChartComp from "@/components/main component/chart component/ChartComp.vue
 import LayananComp from "@/components/main component/layanan component/LayananComp.vue";
 import LocationComp from "@/components/main component/location component/LocationComp.vue";
 import QuoteComp from "@/components/main component/quote component/QuoteComp.vue";
-import FAQComp from "@/components/main component/faq component/FAQComp.vue";
 import FooterComp from "@/components/global component/FooterComp.vue";
 import KerajinanComp from "@/components/main component/kerjinan component/KerajinanComp.vue";
+// import FAQComp from '@/components/main component/faq component/FaqComp.vue';
 
 export default {
   components: {
     KerajinanComp,
     FooterComp,
-    FAQComp,
+    // FAQComp,
     QuoteComp,
     LocationComp,
     LayananComp,

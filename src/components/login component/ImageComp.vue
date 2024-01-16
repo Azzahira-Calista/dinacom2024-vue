31fa402f6e803107afd5f1eef63d3399705cba3e
Tsabitah07 <122654070+Tsabitah07@users.noreply.github.com>
2024-01-16 16:37:25 +0700

    fix [IT] login sized

--- src/components/login component/ImageComp.vue
+++ src/components/login component/ImageComp.vue
@@ -1,6 +1,6 @@
 <template>
     <div>
-        <div class="flex flex-col w-[56.15rem] h-screen">
+        <div class="flex flex-col w-[50.475rem] h-screen">
           <img :src="imgs" alt="Trash Image" class="w-full h-full object-cover" />
       </div>
     </div>

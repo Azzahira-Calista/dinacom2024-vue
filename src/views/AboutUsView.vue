9cbcbeb4df2e8076f8db6810866f14a657fdd7b9
Tsabitah07 <122654070+Tsabitah07@users.noreply.github.com>
2024-01-17 15:14:58 +0700

    feat [IT] goals about us

--- src/views/AboutUsView.vue
+++ src/views/AboutUsView.vue
@@ -1,8 +1,10 @@
 <template>
     <div class="bg-bgColor text-[#303030]">
       <nav-bar-comp/>
-        <AboutCompHero/>
-        <AboutCompMission/>
+      <AboutCompHero/>
+      <AboutCompMission/>
+      <AboutCompGoals/>
+      <FooterComp/>
     </div>
 </template>
 
@@ -10,12 +12,16 @@
     import AboutCompHero from "@/components/about component/AboutCompHero.vue";
     import AboutCompMission from "@/components/about component/AboutCompMission.vue";
     import NavBarComp from "@/components/global component/NavBarComp.vue";
+    import AboutCompGoals from "@/components/about component/AboutCompGoals.vue";
+    import FooterComp from "@/components/global component/FooterComp.vue";
 
     export default {
         components: {
           NavBarComp,
-            AboutCompHero,
-            AboutCompMission
+          AboutCompHero,
+          AboutCompMission,
+          AboutCompGoals,
+          FooterComp
         }
     };
 </script>

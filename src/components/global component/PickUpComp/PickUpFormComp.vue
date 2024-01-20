7de40ee8f84e30d738a22f3c26ad562a1cecd4d8
ratih shinta <rstihshintaaa6@gmail.com>
2024-01-20 10:31:33 +0700

    fix [RS] popup pickup

--- src/components/global component/PickUpComp/PickUpFormComp.vue
+++ src/components/global component/PickUpComp/PickUpFormComp.vue
@@ -1,125 +1,125 @@
 <template>
-  <div v-if="showPopupPickup" class="fixed inset-0 bg-black-opacity-45 backdrop-blur-sm flex justify-center items-center z-30" >
-
-      <div class=" fixed w-screen h-screen items-center justify-center right-[-2rem] bottom-4 flex" >
-
-        <div class="h-[40rem] w-1/2 p-12 bg-bgColor shadow-2xl rounded-[20px] flex flex-col justify-between">
-            <div class="flex flex-col w-[37.5rem] h-[34.5rem] items-center justify-center font-mont bg-bgColor">
-                <h1 class="font-bold text-3xl text-secondary text-start mb-[3rem]">
-                    {{ heading }}
-                </h1>
-                <form @submit.prevent="pickup" action="" method="post" class="flex flex-col">
-                  <!-- mb-[1.875rem] border border-black-->
-                    <input type="text" v-model="dataPickup.detail_location" name="detail_location" id="detail_location" placeholder="Detail Lokasi" class="bg-[#F1F0F0] mb-[1.25rem] h-[3.125rem] w-[29.5rem] px-[1rem] rounded-[0.9375rem] focus:outline-none focus:border-[#31936D]">
-                    <div class="flex h-[3.125rem] w-[29.5rem] items-center justify-between mb-[1.25rem]">
-                        <input type="number" v-model="dataPickup.weight" name="weight" id="weight" placeholder="Berat Perkiraan" class="bg-[#F1F0F0] h-[3.125rem] w-[24.5rem] px-[1rem] rounded-[0.9375rem] focus:outline-none focus:border-[#31936D]">
-                        <div class="flex justify-center items-center text-center bg-[#F1F0F0] h-[3.125rem] w-[4.25rem] px-[1rem] rounded-[0.9375rem] focus:outline-none">
-                        <p>gram</p>
-                        </div>
-                    </div>
-                    <input type="text" v-model="dataPickup.type" name="type" id="type" placeholder="Jenis Sampah" class="bg-[#F1F0F0] mb-[1.25rem] h-[3.125rem] w-[29.5rem] px-[1rem] rounded-[0.9375rem] focus:outline-none focus:border-[#31936D]">
-                    <textarea type="text" v-model="dataPickup.description" name="description" id="description" placeholder="Deskripsi" class="resize-none bg-[#F1F0F0] h-[9.375rem] w-[29.5rem] p-[1rem] rounded-[0.9375rem] focus:outline-none focus:border-[#31936D] text-start"></textarea>
-
-                    <div class="flex h-[3.125rem] w-[29.5rem] my-[1.75rem] items-center justify-between ">
-                        <router-link @click="closePopupPickup" to="/" class="font-mont font-semibold h-[2.75rem] w-[7.5rem] text-center py-[0.625rem] bg-gray-400 font-montserrat font-[w600] text-white text-xl rounded-[0.9375rem]">
-                            Close
-                        </router-link>
-
-                        <button @click="showAlert " type="submit" class="font-mont font-semibold h-[2.75rem] w-[13.5rem] text-center py-[0.625rem] bg-primary font-montserrat font-[w600] text-white text-xl rounded-[0.9375rem]">
-                        Submit
-                        </button>
-                    </div>
-                </form>
-            </div>
+    <div v-if="showPopupPickup" class="fixed inset-0 bg-black-opacity-45 backdrop-blur-sm flex justify-center items-center z-30" >
+  
+        <div class=" fixed w-screen h-screen items-center justify-center right-[-2rem] bottom-4 flex" >
+  
+          <div class="h-[40rem] w-1/2 p-12 bg-bgColor shadow-2xl rounded-[20px] flex flex-col justify-between">
+              <div class="flex flex-col w-[37.5rem] h-[34.5rem] items-center justify-center font-mont bg-bgColor">
+                  <h1 class="font-bold text-3xl text-secondary text-start mb-[3rem]">
+                      {{ heading }}
+                  </h1>
+                  <form @submit.prevent="pickup" action="" method="post" class="flex flex-col">
+                    <!-- mb-[1.875rem] border border-black-->
+                      <input type="text" v-model="dataPickup.detail_location" name="detail_location" id="detail_location" placeholder="Detail Lokasi" class="bg-[#F1F0F0] mb-[1.25rem] h-[3.125rem] w-[29.5rem] px-[1rem] rounded-[0.9375rem] focus:outline-none focus:border-[#31936D]">
+                      <div class="flex h-[3.125rem] w-[29.5rem] items-center justify-between mb-[1.25rem]">
+                          <input type="number" v-model="dataPickup.weight" name="weight" id="weight" placeholder="Berat Perkiraan" class="bg-[#F1F0F0] h-[3.125rem] w-[24.5rem] px-[1rem] rounded-[0.9375rem] focus:outline-none focus:border-[#31936D]">
+                          <div class="flex justify-center items-center text-center bg-[#F1F0F0] h-[3.125rem] w-[4.25rem] px-[1rem] rounded-[0.9375rem] focus:outline-none">
+                          <p>gram</p>
+                          </div>
+                      </div>
+                      <input type="text" v-model="dataPickup.type" name="type" id="type" placeholder="Jenis Sampah" class="bg-[#F1F0F0] mb-[1.25rem] h-[3.125rem] w-[29.5rem] px-[1rem] rounded-[0.9375rem] focus:outline-none focus:border-[#31936D]">
+                      <textarea type="text" v-model="dataPickup.description" name="description" id="description" placeholder="Deskripsi" class="resize-none bg-[#F1F0F0] h-[9.375rem] w-[29.5rem] p-[1rem] rounded-[0.9375rem] focus:outline-none focus:border-[#31936D] text-start"></textarea>
+  
+                      <div class="flex h-[3.125rem] w-[29.5rem] my-[1.75rem] items-center justify-between ">
+                          <router-link @click="closePopupPickup" to="/" class="font-mont font-semibold h-[2.75rem] w-[7.5rem] text-center py-[0.625rem] bg-gray-400 font-montserrat font-[w600] text-white text-xl rounded-[0.9375rem]">
+                              Close
+                          </router-link>
+  
+                          <button @click="showAlert " type="submit" class="font-mont font-semibold h-[2.75rem] w-[13.5rem] text-center py-[0.625rem] bg-primary font-montserrat font-[w600] text-white text-xl rounded-[0.9375rem]">
+                          Submit
+                          </button>
+                      </div>
+                  </form>
+              </div>
+          </div>
         </div>
-      </div>
-  </div>
-</template>
-
-<script>
-import axios from "axios";
-import Swal from 'sweetalert2';
-
-    export default{
-      components: {},
-      props: {
-        showPopupPickup: Boolean,
-      },
-      data() {
-        // const router = useRouter()
-        return {
-          heading: "Detail Pick Up",
-          button_text: "Pick Up",
-          back: "Batal",
-
-          dataPickup: {
-            detail_location: "",
-            weight: "",
-            type: "",
-            description: ""
-          }
-        };
-      },
-      methods: {
-        async pickup(){
-          try {
-            if(!this.dataPickup.detail_location || !this.dataPickup.weight || !this.dataPickup.type || !this.dataPickup.description){
-             alert("Form belum lengkap. Mohon isi semua field.");
-             return;
+    </div>
+  </template>
+  
+  <script>
+  import axios from "axios";
+  import Swal from 'sweetalert2';
+  
+      export default{
+        components: {},
+        props: {
+          showPopupPickup: Boolean,
+        },
+        data() {
+          // const router = useRouter()
+          return {
+            heading: "Detail Pick Up",
+            button_text: "Pick Up",
+            back: "Batal",
+  
+            dataPickup: {
+              detail_location: "",
+              weight: "",
+              type: "",
+              description: ""
             }
-
-            const response = await axios.post("http://127.0.0.1:8000/api/pick-up", this.dataPickup);
-
-            console.log(response);
-            this.dataPickup.detail_location = "";
-            this.dataPickup.weight = 0;
-            this.dataPickup.type = "";
-            this.dataPickup.description = "";
-
-            this.$router.push("/")
-          }catch (error) {
-            console.error(error)
-          }
+          };
         },
-        closePopupPickup(event) {
-          this.$emit("closePopupPickup", event);
-        },
-        showAlert() {
-            Swal.fire({
-                title: "Apakah anda yakin data sudah benar?",
-                text: "Anda akan melakukan pick up",
-                icon: "warning",
-                showCancelButton: true,
-                confirmButtonColor: "#e88a1b",
-                cancelButtonColor: "#d33",
-                confirmButtonText: "Ya, saya yakin!",
-                cancelButtonText: "Batal",
-
-        }).then((result) => {
-            if (result.isConfirmed) {
-                this.$emit("closePopupPickup", event);
-
-            Swal.fire({
-                title: "Berhasil!",
-                text: "Pick up berhasil dilakukan",
-                icon: "success",
-                confirmButtonColor: "#E88A1B",
-            })  
-            } else {
-            Swal.fire({
-                title: "Batal",
-                text: "Pick up dibatalkan",
-                icon: "error",
-                confirmButtonColor: "#E88A1B",
-            });
+        methods: {
+          async pickup(){
+            try {
+              if(!this.dataPickup.detail_location || !this.dataPickup.weight || !this.dataPickup.type || !this.dataPickup.description){
+               alert("Form belum lengkap. Mohon isi semua field.");
+               return;
+              }
+  
+              const response = await axios.post("http://127.0.0.1:8000/api/pick-up", this.dataPickup);
+  
+              console.log(response);
+              this.dataPickup.detail_location = "";
+              this.dataPickup.weight = 0;
+              this.dataPickup.type = "";
+              this.dataPickup.description = "";
+  
+              this.$router.push("/")
+            }catch (error) {
+              console.error(error)
             }
-        });
-        },
-    },
-
-}
-</script>
-
-<style>
-
-</style>
+          },
+          closePopupPickup(event) {
+            this.$emit("closePopupPickup", event);
+          },
+          showAlert() {
+              Swal.fire({
+                  title: "Apakah anda yakin data sudah benar?",
+                  text: "Anda akan melakukan pick up",
+                  icon: "warning",
+                  showCancelButton: true,
+                  confirmButtonColor: "#e88a1b",
+                  cancelButtonColor: "#d33",
+                  confirmButtonText: "Ya, saya yakin!",
+                  cancelButtonText: "Batal",
+  
+          }).then((result) => {
+              if (result.isConfirmed) {
+                  this.$emit("closePopupPickup", event);
+  
+              Swal.fire({
+                  title: "Berhasil!",
+                  text: "Pick up berhasil dilakukan",
+                  icon: "success",
+                  confirmButtonColor: "#E88A1B",
+              })  
+              } else {
+              Swal.fire({
+                  title: "Batal",
+                  text: "Pick up dibatalkan",
+                  icon: "error",
+                  confirmButtonColor: "#E88A1B",
+              });
+              }
+          });
+          },
+      },
+  
+  }
+  </script>
+  
+  <style>
+  
+  </style>
